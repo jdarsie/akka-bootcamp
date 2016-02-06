03d3fba8d8f3c349db9894190451e3cab9fa80bf
jdarsie <javier.darsie@gmail.com>
2016-02-05 23:30:11 -0600

    Done with lesson 1.6

--- src/Unit-1/DoThis/TailActor.cs
+++ src/Unit-1/DoThis/TailActor.cs
@@ -7,8 +7,10 @@ namespace WinTail
 {
     public class TailActor : UntypedActor
     {
-        private readonly StreamReader _fileStreamReader;
+        private readonly string _filePath;
         private readonly IActorRef _reporterActor;
+        private StreamReader _fileStreamReader;
+        private FileObserver _observer;
 
         public TailActor(IActorRef reporterActor, string filePath)
         {
@@ -23,29 +25,45 @@ namespace WinTail
             }
 
             _reporterActor = reporterActor;
+            _filePath = filePath;
+        }
 
+        protected override void PostStop()
+        {
+            _observer.Dispose();
+            _observer = null;
+
+            _fileStreamReader.Close();
+            _fileStreamReader.Dispose();
+
+            base.PostStop();
+        }
+
+        protected override void PreStart()
+        {
             // start watching file for changes
-            var observer = new FileObserver(Self, Path.GetFullPath(filePath));
-            observer.Start();
+            _observer = new FileObserver(Self, Path.GetFullPath(_filePath));
+            _observer.Start();
 
             // open the file stream with shared read/write permissions (so file can be written to while open)
-            var fileStream = new FileStream(Path.GetFullPath(filePath), FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            var fileStream = new FileStream(Path.GetFullPath(_filePath), FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
             _fileStreamReader = new StreamReader(fileStream, Encoding.UTF8);
 
             // read the initial contents of the file and send it to console as first msg
             var text = _fileStreamReader.ReadToEnd();
 
-            Self.Tell(new InitialRead(filePath, text));
+            Self.Tell(new InitialRead(_filePath, text));
         }
 
         protected override void OnReceive(object message)
         {
             if (message is FileWrite)
             {
-                // move file cursor forward
+                // move file cursor forward 
                 // pull results from cursor to end of file and write to output
                 // (this is assuming a log file type format that is append-only)
                 var text = _fileStreamReader.ReadToEnd();
+
                 if (!string.IsNullOrEmpty(text))
                 {
                     _reporterActor.Tell(text);
