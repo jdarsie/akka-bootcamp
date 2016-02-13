17e0f03eaa41b42aad025ddc6ca77b94313b735e
jdarsie <javier.darsie@gmail.com>
2016-02-13 16:38:38 -0600

    Done with lesson 3.1

--- src/Unit-3/DoThis/Actors/GithubCommanderActor.cs
+++ src/Unit-3/DoThis/Actors/GithubCommanderActor.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using Akka.Actor;
+using Akka.Routing;
 
 namespace GithubActors.Actors
 {
     /// <summary>
     /// Top-level actor responsible for coordinating and launching repo-processing jobs
     /// </summary>
-    public class GithubCommanderActor : ReceiveActor
+    public class GithubCommanderActor : ReceiveActor, IWithUnboundedStash
     {
         #region  -- Inner Types --
 
@@ -46,26 +47,58 @@ namespace GithubActors.Actors
 
         private IActorRef _coordinator;
 
+        private int _pendingJobReplies;
+
+        public IStash Stash { get; set; }
+
         public GithubCommanderActor()
         {
-            Receive<CanAcceptJob>(job =>
-            {
-                _canAcceptJobSender = Sender;
-                _coordinator.Tell(job);
-            });
+            Ready();
+        }
 
-            Receive<UnableToAcceptJob>(job => { _canAcceptJobSender.Tell(job); });
+        private void Asking()
+        {
+            // stash any subsequent requests
+            Receive<CanAcceptJob>(job => Stash.Stash());
+
+            Receive<UnableToAcceptJob>(job =>
+            {
+                _pendingJobReplies--;
+
+                if (_pendingJobReplies != 0)
+                {
+                    return;
+                }
+
+                _canAcceptJobSender.Tell(job);
+                BecomeReady();
+            });
 
             Receive<AbleToAcceptJob>(job =>
             {
                 _canAcceptJobSender.Tell(job);
 
-                //start processing messages
-                _coordinator.Tell(new GithubCoordinatorActor.BeginJob(job.Repo));
+                // start processing messages
+                Sender.Tell(new GithubCoordinatorActor.BeginJob(job.Repo));
 
-                //launch the new window to view results of the processing
+                // launch the new window to view results of the processing
                 Context.ActorSelection(ActorPaths.MainFormActor.Path).Tell(new MainFormActor.LaunchRepoResultsWindow(job.Repo, Sender));
+
+                BecomeReady();
             });
+        }
+
+        private void BecomeAsking()
+        {
+            _canAcceptJobSender = Sender;
+            _pendingJobReplies = 3; //the number of routees
+            Become(Asking);
+        }
+
+        private void BecomeReady()
+        {
+            Become(Ready);
+            Stash.UnstashAll();
         }
 
         protected override void PreRestart(Exception reason, object message)
@@ -77,8 +110,29 @@ namespace GithubActors.Actors
 
         protected override void PreStart()
         {
-            _coordinator = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()), ActorPaths.GithubCoordinatorActor.Name);
+            // create three GithubCoordinatorActor instances
+            var c1 = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()), ActorPaths.GithubCoordinatorActor.Name + "1");
+            var c2 = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()), ActorPaths.GithubCoordinatorActor.Name + "2");
+            var c3 = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()), ActorPaths.GithubCoordinatorActor.Name + "3");
+
+            // create a broadcast router who will ask all of them if they're available for work
+            _coordinator =
+                Context.ActorOf(Props.Empty.WithRouter(new BroadcastGroup(
+                    ActorPaths.GithubCoordinatorActor.Path + "1",
+                    ActorPaths.GithubCoordinatorActor.Path + "2",
+                    ActorPaths.GithubCoordinatorActor.Path + "3")));
+
             base.PreStart();
+        }
+
+        private void Ready()
+        {
+            Receive<CanAcceptJob>(job =>
+            {
+                _coordinator.Tell(job);
+
+                BecomeAsking();
+            });
         }
     }
 }
